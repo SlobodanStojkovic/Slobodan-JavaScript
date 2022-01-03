71667ecc54a4de88c67eb3e15455d12b745056de
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-01-03 23:09:49 +0100

    Intro added

---
+++ intro.js
@@ -0,0 +1,26 @@
+/* JavaScript is a dynamic, weakly typed programming language which is compiled at runtime. It can be executed as part of a webpage in a browser or directly on any machine ("host environment").
+
+Weakly typed languages assign types (like "number") to variables (data containters) at runtime - i.e. we cant set the types we want to use in certain places in advance. Only indirectly by making sure we are always working with the correct values. 
+
+JavaScript was created to make webpages more dynamic (e.g. change content on a page directly from inside the browser). 
+
+VARIABLE is a DATA CONTAINER or DATA STORAGE where the value can change.
+
+CONSTANTS are also DATA CONTAINERS or DATA STORAGES where the value MUST NOT CHANGE.
+
+We should use constants as ofthen as possible (i.e. whenever you actually got data that never changes) to be clear about your intentions (in your code).
+
+VARIABLE NAMING vvv
+ALLOWED:
+let userName > Best practice: camelCase
+let ageGroup5 > Only letters and digits
+let $kindOfSpecial > Starting with $ is allowed
+let _internalValue > Starting with _ is allowed
+
+NOT ALLOWED / NOT RECOMMENDED
+
+let user_name > Allowed but bad practice!
+let 21Players > Starting digits not allowed!
+let user-b > No special characters!
+let let > Keywords not allowed!
+*/
